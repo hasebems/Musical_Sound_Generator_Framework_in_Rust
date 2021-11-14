e633c136c28bdd65e585fd078451c51605103720
Hasebe Masahiko <JCA03205@gmail.com>
2021-11-14 17:56:24 +0900

    FIFO追加し、messageはAudio側の関数内で行うように修正

--- src/msgf_if.rs
+++ src/msgf_if.rs
@@ -15,6 +15,7 @@ use crate::general::*;
 //		Class
 //---------------------------------------------------------
 pub struct Msgf {
+    msg_buf: Vec<(u8,usize,u8,u8)>,
     part: Vec<msgf_part::Part>,
     in_number_frames: u32,
 }
@@ -22,6 +23,7 @@ pub struct Msgf {
 impl Msgf {
     pub fn new() -> Self {
         let mut msgf = Self {
+            msg_buf: Vec::new(),
             part: Vec::new(),
             in_number_frames: 0,
         };
@@ -38,25 +40,34 @@ impl Msgf {
             return;
         };
 
-        let pt = &mut self.part[ch];
-        match status {
-            0x80 => pt.note_off(dt2, dt3),
-            0x90 => if dt3 == 0 { pt.note_off(dt2, dt3);} else { pt.note_on(dt2, dt3);},
-            0xb0 => pt.control_change(dt2, dt3),
-            0xc0 => pt.program_change(dt2),
-            0xe0 => {
-                let mut bend: i16 = dt3.into();
-                bend += dt2 as i16*128;
-                bend -= 8192;
-                pt.pitch_bend(bend);
-            }
-            _ => {}
-        };
+        let msg: (u8,usize,u8,u8) = (status, ch, dt2, dt3);
+        self.msg_buf.push(msg);
+    }
+    fn parse_msg(&mut self) {
+        if let Some(msg) = self.msg_buf.pop() {
+            let (status, ch, dt2, dt3) = msg;
+            let pt = &mut self.part[ch];
+            match status {
+                0x80 => pt.note_off(dt2, dt3),
+                0x90 => if dt3 == 0 { pt.note_off(dt2, dt3);} else { pt.note_on(dt2, dt3);},
+                0xb0 => pt.control_change(dt2, dt3),
+                0xc0 => pt.program_change(dt2),
+                0xe0 => {
+                    let mut bend: i16 = dt3.into();
+                    bend += dt2 as i16*128;
+                    bend -= 8192;
+                    pt.pitch_bend(bend);
+                }
+                _ => {}
+            };
+        }
     }
     pub fn process(&mut self,
-        abuf_l: &mut [f32; general::MAX_BUFFER_SIZE],
-        abuf_r: &mut [f32; general::MAX_BUFFER_SIZE],
-        in_number_frames: u32) {
+      abuf_l: &mut [f32; general::MAX_BUFFER_SIZE],
+      abuf_r: &mut [f32; general::MAX_BUFFER_SIZE],
+      in_number_frames: u32) {
+        self.parse_msg();   // MIDI message
+
         if self.in_number_frames != in_number_frames {
             println!("Audio Buffer: {}",in_number_frames);
             self.in_number_frames = in_number_frames;
